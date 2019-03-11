0f996a5d1104fb5e31660949291dfe25e698f2ec
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2019-03-11 19:28:11 +0800

    Revert "ytfh"
    This reverts commit 78822f09012f72ad901cb9747b1d9b12e5fa48ac.

--- session.php
+++ session.php
@@ -12,7 +12,7 @@
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($dbc,'SELECT username FROM users WHERE username = ' . $user_check);
+   $ses_sql = mysqli_query($dbc,"select username from users where username = '$user_check' ");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
